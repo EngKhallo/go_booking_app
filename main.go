77fbda1c44fc85932a18b9c306285e1e1e03d9ab
EngKhallo <engkhalid.ict@gmail.com>
2023-10-14 19:47:07 +0300

    set constant of:number of tickets and remainings

--- main.go
+++ main.go
@@ -3,6 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Welcome to our conference booking application")
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	const remainingTickets = 50
+
+	fmt.Println("Welcome to", conferenceName, "booking application")
+	fmt.Println("We've a total of:", conferenceTickets, "tickets but", remainingTickets, "tickets are still available")
 	fmt.Println("Get your tickets here to attend")
+
 }
