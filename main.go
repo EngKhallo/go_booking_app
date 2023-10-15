8ea153677bd017bb4fd4e00cce0f712a804431af
EngKhallo <engkhalid.ict@gmail.com>
2023-10-16 00:50:04 +0300

    looping through form - to not exit

--- main.go
+++ main.go
@@ -6,30 +6,33 @@ func main() {
 	var conferenceName = "Go Conference"
 	const conferenceTickets = 50
 	var remainingTickets uint = 50
-	var bookings [] string
+	var bookings []string
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We've a total of %v tickets but %v tickets are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var userName string
-	var email string
-	var userTickets uint
-	// ask user for their name: user input
-	fmt.Println("Please enter your name: ")
-	fmt.Scan(&userName)
+	for {
 
-	fmt.Println("Please enter your email: ")
-	fmt.Scan(&email)
-	
-	fmt.Println("enter number of tickets: ")
-	fmt.Scan(&userTickets)
+		var userName string
+		var email string
+		var userTickets uint
+		// ask user for their name: user input
+		fmt.Println("Please enter your name: ")
+		fmt.Scan(&userName)
 
-	remainingTickets = remainingTickets - userTickets
-	bookings = append(bookings, userName)
+		fmt.Println("Please enter your email: ")
+		fmt.Scan(&email)
 
-	fmt.Printf("Thank you %v for booking %v tickets, you'll receive confirmation email at %v\n", userName, userTickets, email)
-	fmt.Printf("%v tickets still remains for %v\n", remainingTickets, conferenceName)
+		fmt.Println("enter number of tickets: ")
+		fmt.Scan(&userTickets)
 
-	fmt.Printf("Booked names are %v", bookings)
+		remainingTickets = remainingTickets - userTickets
+		bookings = append(bookings, userName)
+
+		fmt.Printf("Thank you %v for booking %v tickets, you'll receive confirmation email at %v\n", userName, userTickets, email)
+		fmt.Printf("%v tickets still remains for %v\n", remainingTickets, conferenceName)
+
+		fmt.Printf("Booked names are %v\n", bookings)
+	}
 }
