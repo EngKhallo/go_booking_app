dcd8086eb9ac4cf0706d01bd30048983f8446859
EngKhallo <engkhalid.ict@gmail.com>
2023-10-15 21:34:09 +0300

    user inputs + pointers

--- main.go
+++ main.go
@@ -11,4 +11,18 @@ func main() {
 	fmt.Printf("We've a total of %v tickets but %v tickets are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
+	var userName string
+	var email string
+	var userTickets int
+	// ask user for their name: user input
+	fmt.Println("Please enter your name: ")
+	fmt.Scan(&userName)
+
+	fmt.Println("Please enter your email: ")
+	fmt.Scan(&email)
+	
+	fmt.Println("enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	fmt.Printf("Thank you %v for booking %v tickets, you'll receive confirmation email at %v", userName, userTickets, email)
 }
