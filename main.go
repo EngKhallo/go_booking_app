c5e0df705ffe3d994ec43fa6e1e57dec00702697
EngKhallo <engkhalid.ict@gmail.com>
2023-10-16 00:48:30 +0300

    List of booked prople - Slice/array

--- main.go
+++ main.go
@@ -5,7 +5,8 @@ import "fmt"
 func main() {
 	var conferenceName = "Go Conference"
 	const conferenceTickets = 50
-	const remainingTickets = 50
+	var remainingTickets uint = 50
+	var bookings [] string
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We've a total of %v tickets but %v tickets are still available\n", conferenceTickets, remainingTickets)
@@ -13,7 +14,7 @@ func main() {
 
 	var userName string
 	var email string
-	var userTickets int
+	var userTickets uint
 	// ask user for their name: user input
 	fmt.Println("Please enter your name: ")
 	fmt.Scan(&userName)
@@ -24,5 +25,11 @@ func main() {
 	fmt.Println("enter number of tickets: ")
 	fmt.Scan(&userTickets)
 
-	fmt.Printf("Thank you %v for booking %v tickets, you'll receive confirmation email at %v", userName, userTickets, email)
+	remainingTickets = remainingTickets - userTickets
+	bookings = append(bookings, userName)
+
+	fmt.Printf("Thank you %v for booking %v tickets, you'll receive confirmation email at %v\n", userName, userTickets, email)
+	fmt.Printf("%v tickets still remains for %v\n", remainingTickets, conferenceName)
+
+	fmt.Printf("Booked names are %v", bookings)
 }
